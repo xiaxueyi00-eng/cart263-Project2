b532f0024704145cbd69e04ce3fdf222c5c80362
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2026-04-13 19:40:13 -0400

    do new page

--- Xueyi Xia/js/shared.js
+++ Xueyi Xia/js/shared.js
@@ -1,3 +1,35 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+const geometry = new THREE.SphereGeometry(2, 32, 32);
+const material = new THREE.MeshStandardMaterial({ color: 0x95d4ee });
+const mesh = new THREE.Mesh(geometry, material);
+
+mesh.castShadow = true;
+mesh.receiveShadow = true;
+
+scene.add(mesh);
+
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambient);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
