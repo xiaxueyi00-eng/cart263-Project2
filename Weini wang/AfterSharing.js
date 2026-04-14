ec63c2e48c22d5efd879798a18f87eb0c193f091
bbwinnie <nini200702@gmail.com>
2026-04-14 18:59:54 -0400

    redo the player

--- Weini wang/AfterSharing.js
+++ Weini wang/AfterSharing.js
@@ -22,15 +22,40 @@ let player;
 let cursors;
 
 function preload() {
-
+    this.load.spritesheet("player", "image/16x32 Walk1.png", {
+        frameWidth: 16,
+        frameHeight: 32
+    });
 }
 
 function create() {
 
-    player = this.add.rectangle(100, 300, 40, 40, 0x00ffcc);
-    this.physics.add.existing(player);
+    player = this.physics.add.sprite(100, 300, "player");
+    player.setScale(3);
+    player.setOrigin(0.5, 0.5);
 
-    player.body.setCollideWorldBounds(true);
+    this.anims.create({
+        key: "walkDown",
+        frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: "walkSide",
+        frames: this.anims.generateFrameNumbers("player", { start: 4, end: 7 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: "walkUp",
+        frames: this.anims.generateFrameNumbers("player", { start: 8, end: 11 }),
+        frameRate: 8,
+        repeat: -1
+    });
+
+
 
     cursors = this.input.keyboard.createCursorKeys();
 }
@@ -39,16 +64,38 @@ function update() {
     player.body.setVelocity(0);
 
     if (cursors.left.isDown) {
-        player.body.setVelocityX(-200);
+        player.setVelocityX(-120);
+
+        player.anims.play("walkSide", true);
+
+        player.setFlipX(true); // 👈 向左翻转
     }
     else if (cursors.right.isDown) {
-        player.body.setVelocityX(200);
-    }
+        player.setVelocityX(120);
 
-    if (cursors.up.isDown) {
-        player.body.setVelocityY(-200);
+        player.anims.play("walkSide", true);
+
+        player.setFlipX(false); // 👈 正常（向右）
     }
     else if (cursors.down.isDown) {
-        player.body.setVelocityY(200);
+        player.setVelocityY(120);
+
+        player.anims.play("walkDown", true);
+
+        player.setFlipX(false); // 防止残留翻转
+    }
+    else if (cursors.up.isDown) {
+        player.setVelocityY(-120);
+
+        player.anims.play("walkUp", true);
+
+        player.setFlipX(false);
+    }
+
+
+    else {
+        player.setVelocity(0);
+
+        player.anims.stop();
     }
 }
