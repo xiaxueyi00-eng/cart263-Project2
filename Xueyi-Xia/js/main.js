56334ab02b1f26b8f4c6965b42dfe62ef6fd40b2
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2026-04-15 16:05:43 -0400

    upload the code

--- Xueyi-Xia/js/main.js
+++ Xueyi-Xia/js/main.js
@@ -1,6 +1,6 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.182.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.182.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.182.0/examples/jsm/loaders/GLTFLoader.js";
 /* =========================
    STAR BACKGROUND (2D CANVAS)
 ========================= */
